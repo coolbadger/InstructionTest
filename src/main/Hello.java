7998fd211c9d1ac4fdf665329da63c9fed177e04
lekoxnfx <lekoxnfx@qq.com>
2016-01-07 12:07:18 +0800

    aaa2

--- src/main/Hello.java
+++ src/main/Hello.java
@@ -5,7 +5,7 @@ package main;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello heheda by lekoxnfx");
+        System.out.println("Hello heheda by lekoxnfx2");
 
     }
 }
