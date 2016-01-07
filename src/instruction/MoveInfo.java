5e288afa474f3bc97e1b664f21834a3bb636f521
Badger <hanbadger@gmail.com>
2016-01-07 15:07:46 +0800

    Add JsonProcess Class 生成Get Set

--- src/instruction/MoveInfo.java
+++ src/instruction/MoveInfo.java
@@ -6,24 +6,153 @@ import java.util.Date;
  * Created by Badger on 16/1/7.
  */
 public class MoveInfo {
-    public String batchId;                 //作业批次号
-    public int moveId;                     //作业编号
-    public int movekind;                   //作业类型
-    public String unitId;                  //箱编号
+    private String batchId;                 //作业批次号
+    private int moveId;                     //作业编号
+    private int movekind;                   //作业类型
+    private String unitId;                  //箱编号
 
-    public UnitPosition eFromPositon;      //计划提箱位置
-    public UnitPosition eToPosition;       //计划放箱位置
-    public UnitPosition aFromPosition;     //实际提箱位置
-    public UnitPosition aToPosition;       //实际放箱位置
+    private UnitPosition eFromPositon;      //计划提箱位置
+    private UnitPosition eToPosition;       //计划放箱位置
+    private UnitPosition aFromPosition;     //实际提箱位置
+    private UnitPosition aToPosition;       //实际放箱位置
 
-    public String fetchCHE;                //Fetch设备
-    public String carryCHE;                //Carry设备
-    public String putCHE;                  //Put设备
+    private String fetchCHE;                //Fetch设备
+    private String carryCHE;                //Carry设备
+    private String putCHE;                  //Put设备
 
-    public Date dispatchTime;              //指令派发时间
-    public Date fetchTime;                 //Fetch确认时间
-    public Date carryTime;                 //Carry确认时间
-    public Date putTime;                   //Put确认时间
+    private Date dispatchTime;              //指令派发时间
+    private Date fetchTime;                 //Fetch确认时间
+    private Date carryTime;                 //Carry确认时间
+    private Date putTime;                   //Put确认时间
 
-    public int state;                      //状态
+    private int state;                      //状态
+
+    public String getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(String batchId) {
+        this.batchId = batchId;
+    }
+
+    public int getMoveId() {
+        return moveId;
+    }
+
+    public void setMoveId(int moveId) {
+        this.moveId = moveId;
+    }
+
+    public int getMovekind() {
+        return movekind;
+    }
+
+    public void setMovekind(int movekind) {
+        this.movekind = movekind;
+    }
+
+    public String getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(String unitId) {
+        this.unitId = unitId;
+    }
+
+    public UnitPosition geteFromPositon() {
+        return eFromPositon;
+    }
+
+    public void seteFromPositon(UnitPosition eFromPositon) {
+        this.eFromPositon = eFromPositon;
+    }
+
+    public UnitPosition geteToPosition() {
+        return eToPosition;
+    }
+
+    public void seteToPosition(UnitPosition eToPosition) {
+        this.eToPosition = eToPosition;
+    }
+
+    public UnitPosition getaFromPosition() {
+        return aFromPosition;
+    }
+
+    public void setaFromPosition(UnitPosition aFromPosition) {
+        this.aFromPosition = aFromPosition;
+    }
+
+    public UnitPosition getaToPosition() {
+        return aToPosition;
+    }
+
+    public void setaToPosition(UnitPosition aToPosition) {
+        this.aToPosition = aToPosition;
+    }
+
+    public String getFetchCHE() {
+        return fetchCHE;
+    }
+
+    public void setFetchCHE(String fetchCHE) {
+        this.fetchCHE = fetchCHE;
+    }
+
+    public String getCarryCHE() {
+        return carryCHE;
+    }
+
+    public void setCarryCHE(String carryCHE) {
+        this.carryCHE = carryCHE;
+    }
+
+    public String getPutCHE() {
+        return putCHE;
+    }
+
+    public void setPutCHE(String putCHE) {
+        this.putCHE = putCHE;
+    }
+
+    public Date getDispatchTime() {
+        return dispatchTime;
+    }
+
+    public void setDispatchTime(Date dispatchTime) {
+        this.dispatchTime = dispatchTime;
+    }
+
+    public Date getFetchTime() {
+        return fetchTime;
+    }
+
+    public void setFetchTime(Date fetchTime) {
+        this.fetchTime = fetchTime;
+    }
+
+    public Date getCarryTime() {
+        return carryTime;
+    }
+
+    public void setCarryTime(Date carryTime) {
+        this.carryTime = carryTime;
+    }
+
+    public Date getPutTime() {
+        return putTime;
+    }
+
+    public void setPutTime(Date putTime) {
+        this.putTime = putTime;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
 }
--- src/instruction/UnitPosition.java
+++ src/instruction/UnitPosition.java
@@ -4,8 +4,41 @@ package instruction;
  * Created by Badger on 16/1/7.
  */
 public class UnitPosition {
-    public String area;        //场区
-    public String bay;         //贝
-    public String lay;         //位
-    public String tie;         //层
+    private String area;        //场区
+    private String bay;         //贝
+    private String lay;         //位
+    private String tie;         //层
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getBay() {
+        return bay;
+    }
+
+    public void setBay(String bay) {
+        this.bay = bay;
+    }
+
+    public String getLay() {
+        return lay;
+    }
+
+    public void setLay(String lay) {
+        this.lay = lay;
+    }
+
+    public String getTie() {
+        return tie;
+    }
+
+    public void setTie(String tie) {
+        this.tie = tie;
+    }
+
 }
