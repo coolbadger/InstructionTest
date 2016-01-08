e9e7f7f22f59757507c0cb0771ce03496e26f42c
lekoxnfx <lekoxnfx@qq.com>
2016-01-08 22:41:01 +0800

    增加三个空线程

---
+++ src/main/SendWIThread.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by lekoxnfx on 16/1/8.
+ */
+public class SendWIThread extends Thread{ //生成发送的指令信息
+    @Override
+    public void run() {
+        super.run();
+    }
+}
