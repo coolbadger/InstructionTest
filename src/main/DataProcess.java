f67118f394b308abde5b92209fc0ac4ffe693ada
Badger <hanbadger@gmail.com>
2016-01-08 23:43:04 +0800

    添加数据处理类 DataProcess 修改Readme

---
+++ src/main/DataProcess.java
@@ -0,0 +1,48 @@
+package main;
+
+import instruction.JsonProcess;
+import instruction.LoadConfig;
+import instruction.MoveInfo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Badger on 16/1/8.
+ */
+public class DataProcess {
+
+    //初始化全局列表
+    public synchronized void initGlobalList() {
+        GlobalData.GLOBAL_MOVEINFO_LIST = new HashMap<String, MoveInfo>();
+
+        List<MoveInfo> moveInfoList = new LoadConfig().getInstructions();
+        this.setGlobalList(moveInfoList);
+    }
+
+    //根据返回Json,更新全局列表
+    public synchronized void updateGlobalList(String jsonStr) {
+        List<MoveInfo> moveInfoList = new JsonProcess().getMoveInfos(jsonStr);
+        this.setGlobalList(moveInfoList);
+    }
+
+    //根据选中指令列表,生成指令Json字符串
+    public synchronized String genInstructions(List<String> keyList) {
+        List<MoveInfo> moveInstructions = new ArrayList<MoveInfo>();
+        for (String key : keyList) {
+            MoveInfo moveInfo = GlobalData.GLOBAL_MOVEINFO_LIST.get(key);
+            if (moveInfo != null)
+                moveInstructions.add(moveInfo);
+        }
+        return new JsonProcess().getJsonStr(moveInstructions);
+    }
+
+    private void setGlobalList(List<MoveInfo> moveInfoList) {
+        for (MoveInfo moveInfo : moveInfoList) {
+            String gkey = moveInfo.getBatchId() + "@" + moveInfo.getMoveId();
+            GlobalData.GLOBAL_MOVEINFO_LIST.put(gkey, moveInfo);
+        }
+    }
+}
--- src/main/GlobalData.java
+++ src/main/GlobalData.java
@@ -13,22 +13,16 @@ import java.util.Map;
 public class GlobalData {
     public static Map<String, MoveInfo> GLOBAL_MOVEINFO_LIST = new HashMap<String, MoveInfo>();
 
-
-
-
-
     //监听器及其设置
     private static List<IGlobalData> globalDataChangeListeners = new ArrayList<IGlobalData>();
 
-    public static void addGlobalDataChangeListener(IGlobalData iGlobalData){
+    public static void addGlobalDataChangeListener(IGlobalData iGlobalData) {
         globalDataChangeListeners.add(iGlobalData);
     }
 
-    private void notifyChange(){//通知所有监听者
-        for(IGlobalData iGlobalData:globalDataChangeListeners){
+    private void notifyChange() {//通知所有监听者
+        for (IGlobalData iGlobalData : globalDataChangeListeners) {
             iGlobalData.globalDataChanged();
         }
     }
-
-
 }
