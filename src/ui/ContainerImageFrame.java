26fcafd4f04fa0980041c41d2e347faacab04d96
dingharry <ding4work@163.com>
2016-03-21 12:13:44 +0800

    集装箱展示画面。第二次提交

--- src/ui/ContainerImageFrame.java
+++ src/ui/ContainerImageFrame.java
@@ -9,7 +9,8 @@ import java.awt.*;
 public class ContainerImageFrame extends JFrame{
     private JPanel contentPanel;
     private JLabel boardLabel;
-    private JPanel eastPanel;
+    private JScrollPane scrollPane;
+    private JPanel forScrolPanel;
 
 //    ContainerImageFrame(){initComponents();}
 
@@ -20,40 +21,78 @@ public class ContainerImageFrame extends JFrame{
         this.setLayout(new BorderLayout());
         this.setSize(1200,800);
         //甲板字样
-        boardLabel = new JLabel("甲板");
-        boardLabel.setFont(new Font("宋体",Font.BOLD,20));
+        boardLabel = new JLabel();
+        boardLabel.setFont(new Font("宋体",Font.BOLD,30));
         boardLabel.setHorizontalAlignment(JLabel.RIGHT);
+        boardLabel.setText("    甲板");
 
-        eastPanel= new JPanel();
-        eastPanel.setLayout(new BorderLayout());
-        eastPanel.setPreferredSize(new Dimension(100,0));
-        eastPanel.add(boardLabel,BorderLayout.WEST);
         //集装箱panel
         contentPanel = new JPanel();
-        contentPanel.setLayout(new GridLayout(2,1,1,20));
+        contentPanel.setLayout(new GridLayout(2,1,1,10));
         contentPanel.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
-//        contentPanel.setBackground(Color.GREEN);
         contentPanel.setSize(new Dimension(900,900));
         //画集装箱
-        DrawContainers drawContainersAbove = new DrawContainers(rowAbove,colAbove);
-        DrawContainers drawContainersUnder = new DrawContainers(rowUnder,colUnder);
+        DrawContainers drawContainersAbove = new DrawContainers(rowAbove,colAbove,true);
+        DrawContainers drawContainersUnder = new DrawContainers(rowUnder,colUnder,false);
         contentPanel.add(drawContainersAbove);
         contentPanel.add(drawContainersUnder);
 
-        this.add(eastPanel,BorderLayout.EAST);
-        this.add(contentPanel,BorderLayout.CENTER);
+        //滚动条panel
+        forScrolPanel = new JPanel();
+        forScrolPanel.setLayout(new BorderLayout(0,0));
+        forScrolPanel.add(boardLabel,BorderLayout.WEST);
+        forScrolPanel.add(contentPanel,BorderLayout.CENTER);
+
+        scrollPane = new JScrollPane();
+        scrollPane.setViewportView(forScrolPanel);
+        this.add(scrollPane);
         this.setLocationRelativeTo(null);
 
     }
     //画集装箱类
     class DrawContainers extends JPanel{
-        public  DrawContainers(int row,int col){
-            this.setLayout(new GridLayout(row,col,1,1));
-            JButton[][] btn = new JButton[row][col];
-            for(int i=0;i<row;i++){
-                for(int j=0;j<col;j++){
-                    btn[i][j]= new JButton(new String("<html>"+"属性1"+"<br>属性2</html>"));
-                    btn[i][j].setFont(new Font("",Font.BOLD,8));
+        public  DrawContainers(int row,int col,boolean underOrAbove){//underOrAbove用来判断是甲板上下以便于draw排号和层号,true代表甲板上
+            this.setLayout(new GridLayout(row+1,col+1,10,5));
+
+            //生成排数的字符串.偶数放在前面技术放在后面
+            StringBuilder stringRes = new StringBuilder("");
+            StringBuilder tempStr;
+            for(int i = 1;i <= col; i++){
+                tempStr = new StringBuilder(String.format("%02d",i));
+                if(i % 2 == 0){
+                    stringRes = tempStr.append(stringRes);
+                }else{
+                    stringRes = stringRes.append(tempStr);
+                }
+            }
+
+            //画集装箱
+            JButton[][] btn = new JButton[row+1][col+1];
+            for(int i=0;i<=row;i++){
+                for(int j=0;j<=col;j++){
+                    btn[i][j] = new JButton();
+                    btn[i][j].setPreferredSize(new Dimension(80,40));
+                    btn[i][j].setFont(new Font("",Font.BOLD,10));
+
+                    if(i== row && j == 0){
+                        btn[i][j] = new JButton(new String(""));
+                        btn[i][j].setContentAreaFilled(false);//jbutton透明
+                        btn[i][j].setBorderPainted(false);//jbutton透明
+                    }else if(i == row){
+                        btn[i][j] = new JButton(stringRes.substring(2*(j-1),2*(j-1)+2));
+                        btn[i][j].setContentAreaFilled(false);//jbutton透明
+                        btn[i][j].setBorderPainted(false);//jbutton透明
+                    }else if(j == 0){
+                        if(underOrAbove == true){
+                            btn[i][j] = new JButton(new String((row - i)*2+80 + ""));//甲板上的层数
+                        }else {
+                            btn[i][j] = new JButton(String.format("%02d",(row - i)*2));//甲板下的层数
+                        }
+                        btn[i][j].setContentAreaFilled(false);//jbutton透明
+                        btn[i][j].setBorderPainted(false);//jbutton透明
+                    }else{
+                        btn[i][j].setText("<html>"+"属性1"+"<br>属性2</html>");
+                    }
                     add(btn[i][j]);
                 }
             }
--- src/ui/VesselImageFrame.java
+++ src/ui/VesselImageFrame.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.QuadCurve2D;
 import java.awt.geom.Rectangle2D;
 import java.util.*;
 
@@ -16,22 +17,68 @@ import java.util.*;
 public class VesselImageFrame extends JFrame {
     private JPanel topPanel;
     private JPanel contentPanel;
+    private JPanel forScrollPanel;
     private JLabel label;
     private JTextField textField;
     private JButton button;
-    //船体四个点的坐标
-    private  int[] SHIPX={0,150,850,1000};
-    private  int[] SHIPY={250,500,500,250};
+    private int MaxTierUnderForDrawBoat = 0;//用来统计甲板下集装箱的最大层数以便于画船体
+    private int MacTierAboveForDrawString = 0;//用来统计甲板下集装箱的最大层数以便于写集装箱上方的文字
+    private int bayCount;   //倍数
+    int boxWidth = 30; //集装箱宽度固定值40
+    int boxHeight =  15;   //集装箱高度就是15
+    int gap = 15; //倍之间的间隔
+    int scrollwidth;
+    //船体四个点的坐标(程序中对四个点进行过更新)
+     int[] SHIPX={20,150,1150,1280};
+     int[] SHIPY={200,450,450,200};
+
+    HashSet<Integer> Bay = new HashSet<Integer>();  //统计倍数总数
+    Map<Integer,Integer> MaxTierUnder = new HashMap<Integer, Integer>(); //记录甲板下每个倍最高层《倍号，层数》
+    Map<Integer,Integer> MaxTierAbove = new HashMap<Integer, Integer>(); //记录甲板上最高层
+
+
 
     VesselImageFrame() {initComponents();}
 
     private void initComponents(){
         Container c = getContentPane();
-        c.setLayout(new BorderLayout(10,10));
+//        c.setLayout(new BorderLayout(10,10));
         this.setTitle("侧视图");
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setSize(1200,800);
+        this.setSize(1400,800);
         this.setLocationRelativeTo(null);
+
+        java.util.List<VesselStructureInfo> vesselStructureInfos = ImportData.vesselStructureInfoList;
+        Integer[] rowData = new Integer[3];
+        for (VesselStructureInfo vesselStructureInfo : vesselStructureInfos) {
+            //                rowData[0] = vesselStructureInfo.getVHTID();      //舱次
+            rowData[0] = vesselStructureInfo.getVBYBAYID();     //倍
+            rowData[1] = vesselStructureInfo.getVTRTIERNO();    //层
+            //                rowData[2] = vesselStructureInfo.getVRWROWNO();     //排
+            Bay.add(rowData[0]); //统计倍数
+            if( rowData[1] < 80 &&  rowData[1] >= 0){
+                if(!MaxTierUnder.containsKey(rowData[0]) || MaxTierUnder.get(rowData[0]) < rowData[1]){
+                    MaxTierUnder.put(rowData[0], rowData[1]/2);
+                }
+                if(MaxTierUnderForDrawBoat < rowData[1]/2) MaxTierUnderForDrawBoat = rowData[1]/2;
+
+            }else if(rowData[1] > 80){
+                if(!MaxTierAbove.containsKey(rowData[0]) ||MaxTierAbove.get(rowData[0]) < rowData[1]){
+                    MaxTierAbove.put(rowData[0], (rowData[1]-80)/2);
+                }
+                if(MacTierAboveForDrawString < (rowData[1]-80)/2) MacTierAboveForDrawString = (rowData[1]-80)/2;
+            }
+        }
+        bayCount = Bay.size();
+        SHIPY[0] = SHIPY[1] - (MaxTierUnderForDrawBoat + 3) * 15;
+        SHIPY[3] = SHIPY[2] - (MaxTierUnderForDrawBoat + 3) * 15;
+        SHIPX[2] = SHIPX[1] + (bayCount+2)*boxWidth + gap*(bayCount/2);//+2为了有空隙空间
+        SHIPX[3] = SHIPX[2] +130;
+        scrollwidth = (int)((SHIPX[2]-SHIPX[1])*1.5+gap*(bayCount/2));
+
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
         //顶部Panel
         topPanel = new JPanel();
@@ -48,7 +95,7 @@ public class VesselImageFrame extends JFrame {
             button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if(!textField.getText().matches("[0-9]+") || Integer.valueOf(textField.getText()) % 2 == 0){
+                    if(!textField.getText().matches("[0-9]+") || Integer.valueOf(textField.getText()) % 2 == 0){//判断输入是否合法。为奇数整数
                         JFrame dialogBackground = new JFrame();
                         dialogBackground.setSize(300,300);
                         dialogBackground.setTitle("输入错误");
@@ -56,19 +103,26 @@ public class VesselImageFrame extends JFrame {
                         dialogBackground.setLocationRelativeTo(null);
                         dialogBackground.setVisible(true);
                     }else {
-                        int bayID = Integer.valueOf(textField.getText());
+                        int bayID = Integer.valueOf(textField.getText());//获取倍数
                         java.util.List<VesselStructureInfo> vesselStructureInfos = ImportData.vesselStructureInfoList;
                         Integer[] rowData = new Integer[3];
                         int maxTierUnder = 0;
                         int maxTierAbove = 0;
-                        int maxCol = 0;
+                        int maxColUnder = 0;
+                        int maxColAbove = 0;
                         for (VesselStructureInfo vesselStructureInfo : vesselStructureInfos) {
                             //                rowData[0] = vesselStructureInfo.getVHTID();      //舱次
                             rowData[0] = vesselStructureInfo.getVBYBAYID();     //倍
                             rowData[1] = vesselStructureInfo.getVTRTIERNO();    //层
                             rowData[2] = vesselStructureInfo.getVRWROWNO();     //排
                             if( rowData[0] == bayID){
-                                if(maxCol < rowData[2])maxCol = rowData[2];
+                                //记录甲板上下的最高排数
+                                if( rowData[1] < 80 &&  rowData[1] >= 0 && maxColUnder < rowData[2]){
+                                    maxColUnder = rowData[2];
+                                }else if(rowData[1] > 80 && maxColAbove <rowData[2]){
+                                    maxColAbove = rowData[2];
+                                }
+                                //记录甲板上下的最高层数
                                 if( rowData[1] < 80 &&  rowData[1] >= 0 && maxTierUnder < rowData[1]/2){
                                     maxTierUnder = rowData[1]/2;
                                 }else if(rowData[1] > 80 && maxTierAbove < (rowData[1]-80)/2){
@@ -76,26 +130,40 @@ public class VesselImageFrame extends JFrame {
                                 }
                             }
                         }
-                        ContainerImageFrame containerImageFrame = new ContainerImageFrame();
-                        containerImageFrame.initComponents(bayID,maxTierUnder,maxCol,maxTierAbove,maxCol);
+                        ContainerImageFrame containerImageFrame = new ContainerImageFrame(); //集装箱倍位概图界面
+                        containerImageFrame.initComponents(bayID,maxTierAbove,maxColAbove,maxTierUnder,maxColUnder);
                         containerImageFrame.setVisible(true);
                     }
                 }
             });
         }
+
+
         //添加顶部panel
         topPanel.add(label);
         topPanel.add(textField);
         topPanel.add(button);
-        c.add("North",topPanel);
 
-        //概图panel
         contentPanel = new JPanel();
-        DrawShip drawShip = new DrawShip();  //画船
         DrawContainers drawContainers = new DrawContainers();   //画集装箱
+        DrawShip drawShip = new DrawShip();  //画船
+
+        drawContainers.setVisible(true);
         contentPanel.add(drawShip);
         contentPanel.add(drawContainers);
-        c.add("Center",contentPanel);
+//        SHIPX[2] = SHIPX[1] + (drawContainers.getbayCount()+2)*boxWidth + gap*(bayCount/2);//+2为了有空隙空间
+//        SHIPX[3] = SHIPX[2] +130;
+//        contentPanel.setPreferredSize(new Dimension(scrollwidth,700));
+        contentPanel.setPreferredSize(new Dimension((int)((SHIPX[2] - SHIPX[1])*1.5),700));
+//        contentPanel.setPreferredSize(new Dimension(1800,600));
+        scrollPane.setViewportView(contentPanel);
+
+        forScrollPanel = new JPanel();
+        forScrollPanel.setLayout(new BorderLayout(10,10));
+        forScrollPanel.add("North",topPanel);
+        forScrollPanel.add("Center",scrollPane);
+
+        c.add(forScrollPanel);
     }
 
     //画船体
@@ -104,11 +172,22 @@ public class VesselImageFrame extends JFrame {
             super.paintComponent(g);
             Graphics2D drawShip;
             drawShip=(Graphics2D)g;
-            drawShip.setColor(Color.BLACK); //设置弧形的颜色为黑色
+            drawShip.setColor(Color.BLUE); //设置弧形的颜色为黑色
             drawShip.setStroke(new BasicStroke(5)); //船的粗细
-            drawShip.drawPolygon(SHIPX,SHIPY,4);    //画船四边形
+
+//            drawShip.drawPolygon(SHIPX,SHIPY,4);    //画船四边形
+            drawShip.drawLine(SHIPX[0],SHIPY[0],SHIPX[3],SHIPY[3]);//画甲板线
+            drawShip.drawLine(SHIPX[1],SHIPY[1],SHIPX[2],SHIPY[2]);//画船底线
+            QuadCurve2D curveLeft = new QuadCurve2D.Double(SHIPX[0],SHIPY[0],SHIPX[1]+20,SHIPY[1]-100,SHIPX[1],SHIPY[1]);//画船的左侧线条
+            QuadCurve2D curveRight = new QuadCurve2D.Double(SHIPX[2],SHIPY[2],SHIPX[2]-20,SHIPY[2]-100,SHIPX[3],SHIPY[3]);//画船的右侧线条
+            drawShip.draw(curveLeft);
+            drawShip.draw(curveRight);
+
+            drawShip.setFont(new Font("",Font.BOLD,30));
+            drawShip.drawString("船头",SHIPX[0],SHIPY[1]);
             this.setOpaque(false);  //透明
-            this.setBounds(100,100,1000,1000);
+            this.setBounds(100,100,1500,1500);
+//            this.setPreferredSize(new Dimension((int)((SHIPX[3] - SHIPX[0])*1.5),600));
         }
     }
 
@@ -120,50 +199,32 @@ public class VesselImageFrame extends JFrame {
             drawboxes = (Graphics2D)g;
             drawboxes.setColor(Color.BLACK);
             drawboxes.setBackground(null);
-            this.setBounds(100,100,1000,1000);
+            drawboxes.setStroke(new BasicStroke(2));//集装箱粗细
+            drawboxes.setFont(new Font("",Font.BOLD,15));
+            this.setBounds(100,100,1500,1500);
             this.setOpaque(false);
 
-            java.util.List<VesselStructureInfo> vesselStructureInfos = ImportData.vesselStructureInfoList;
-            int BAYCount;   //倍数
-            HashSet<Integer> Bay = new HashSet<Integer>();  //统计倍数
-            Map<Integer,Integer> MaxTierUnder = new HashMap<Integer, Integer>(); //记录甲板下最高层《倍号，层数》
-            Map<Integer,Integer> MaxTierAbove = new HashMap<Integer, Integer>(); //记录甲板上最高层
-            Integer[] rowData = new Integer[3];
-                for (VesselStructureInfo vesselStructureInfo : vesselStructureInfos) {
-    //                rowData[0] = vesselStructureInfo.getVHTID();      //舱次
-                    rowData[0] = vesselStructureInfo.getVBYBAYID();     //倍
-                    rowData[1] = vesselStructureInfo.getVTRTIERNO();    //层
-    //                rowData[2] = vesselStructureInfo.getVRWROWNO();     //排
-                    Bay.add(rowData[0]); //统计倍数
-                    if( rowData[1] < 80 &&  rowData[1] >= 0){
-                        if(!MaxTierUnder.containsKey(rowData[0]) || MaxTierUnder.get(rowData[0]) < rowData[1]){
-                            MaxTierUnder.put(rowData[0], rowData[1]/2);
-                        }
-                    }else if(rowData[1] > 80){
-                        if(!MaxTierAbove.containsKey(rowData[0]) ||MaxTierAbove.get(rowData[0]) < rowData[1]){
-                            MaxTierAbove.put(rowData[0], (rowData[1]-80)/2);
-                        }
-                    }
-                }
-            BAYCount = Bay.size();
-            int boxWidth = (SHIPX[2] - SHIPX[1]) / (BAYCount + 4); //计算每个集装箱多宽比较合适，850和150船底坐标，+4为了留有一定空隙
-            int boxHeight = (SHIPY[1] - SHIPY[0]) / 15;   //假设甲板下最大就是15个集装箱
-            for(int i = 0 ; i < BAYCount;i++) {
+
+
+//            int boxWidth = (SHIPX[2] - SHIPX[1]) / (BAYCount + 10); //计算每个集装箱多宽比较合适，+10为了留有一定空隙
+//            int gap = ((SHIPX[2] - SHIPX[1])-boxWidth*BAYCount)/15;
+            for(int i = 0 ; i < bayCount;i++) {
                 //添加倍的文字
                 if(i%2==0){
-                    drawboxes.drawString(i*2+1+"B",SHIPX[1]+ boxWidth * (i+1) + (i/2)*5,SHIPY[1] + 15);
-                    drawboxes.drawString(i*2+1+"A",SHIPX[1]+ boxWidth * (i+1) + (i/2)*5,MaxTierAbove.get(2+1)*boxHeight-50);//写成了固定值
+                    drawboxes.drawString(i*2+1+"B",SHIPX[1]+ boxWidth * (i+1) + (i/2)*gap,SHIPY[1] + 20);
+                    drawboxes.drawString(i*2+1+"A",SHIPX[1]+ boxWidth * (i+1) + (i/2)*gap,SHIPY[1]-(MaxTierUnderForDrawBoat + 3)*boxHeight - (MacTierAboveForDrawString + 3) * boxHeight);
                 }
                 //画集装箱
                 for(int j=0;j<MaxTierUnder.get(i*2+1);j++){
-                    Rectangle2D rectangle2D = new Rectangle2D.Double(SHIPX[1]+ boxWidth * (i+1) + (i/2)*5,SHIPY[1] - boxHeight*(j+1)-5,boxWidth,boxHeight);
+                    Rectangle2D rectangle2D = new Rectangle2D.Double(SHIPX[1]+ boxWidth * (i+1) + (i/2)*gap,SHIPY[1] - boxHeight*(j+1)-5,boxWidth,boxHeight);
                     drawboxes.draw(rectangle2D);
                 }
                 for(int j=0;j<MaxTierAbove.get(i*2+1);j++){
-                    Rectangle2D rectangle2D = new Rectangle2D.Double(SHIPX[1]+ boxWidth * (i+1) + (i/2)*5,SHIPY[0] - boxHeight*(j+1)-5,boxWidth,boxHeight);
+                    Rectangle2D rectangle2D = new Rectangle2D.Double(SHIPX[1]+ boxWidth * (i+1) + (i/2)*gap,SHIPY[0] - boxHeight*(j+1)-5,boxWidth,boxHeight);
                     drawboxes.draw(rectangle2D);
                 }
             }
         }
+
     }
 }
